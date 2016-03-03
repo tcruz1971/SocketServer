85e5c2a776c082820d258644f5bd63004a600561
Agostinho Ourique Cruz <tcruz1971@gmail.com>
2016-03-03 11:23:57 +0100

    First Commit

---
+++ src/com/socketserver/cruz/SocketServer.java
@@ -0,0 +1,26 @@
+package com.socketserver.cruz;
+
+import java.net.*;
+import java.io.*;
+ 
+public class SocketServer {
+    public static void main(String[] args) throws IOException {
+ 
+    if (args.length != 1) {
+        System.err.println("Usage: java SocketServer <port number>");
+        System.exit(1);
+    }
+ 
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+         
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
+            while (listening) {
+                new SocketServerThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
